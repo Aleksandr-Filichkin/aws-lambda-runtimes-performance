f12faa7a7b7b08ca13c83ca94915d8e0b1733b14
The Sharp Ninja <ninja@thesharp.ninja>
2021-11-02 12:59:39 -0500

    Updated to .Net 5.0 and Ahead of time compilation for Linux-x64

---
+++ dotnet-lambda/src/DotNetFunction/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNetFunction
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Run me with AWS Lambda.");
+        }
+    }
+}
